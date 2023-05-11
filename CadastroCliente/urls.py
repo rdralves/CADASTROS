e6cf9644e8cb5e2a640499e0465188be5435517a
Rodrigo rdr <rdr.alves@gmail.com>
2023-05-10 22:15:29 -0300

    criação de novas paginas

--- CadastroCliente/urls.py
+++ CadastroCliente/urls.py
@@ -3,5 +3,8 @@ from CadastroCliente import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('clientes/', views.listar_clientes, name='clientes')
+    path('clientes/', views.listar_clientes, name='clientes'),
+    path('profissao/', views.profissao, name='profissao'),
+    path('cliente', views.detalhar_cliente, name='detalhar')
+
 ]
--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from CadastroCliente.models import Cliente
+from CadastroCliente.models import Cliente, Profissao
 
 # Create your views here.
 def index(request):
@@ -14,4 +14,23 @@ def index(request):
 
 
 def listar_clientes(request):
-    return render(request, 'listar_clientes.html')
+    lista_profissao = Profissao.objects.all()
+    lista_clientes = Cliente.objects.all()
+    context = {
+        "clientes": lista_clientes,
+        'profissoes': lista_profissao
+    }
+    return render(request, 'listar_clientes.html', context)
+
+
+def profissao(request):
+    lista_profissao = Profissao.objects.all()
+    context = {
+        'profissoes': lista_profissao
+    }
+
+    return render(request, 'listar_profissao.html', context)
+
+
+def detalhar_cliente(request):
+    return render(request, 'cliente_detalhe.html')
